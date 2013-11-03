d8a695cbb1589391fecd9d9ceefb622d5c7e7065
Vladimír Vondruš <mosra@centrum.cz>
2013-11-03 08:37:33 +0100

    WINAPI I hate you.

--- src/TextSprite.cpp
+++ src/TextSprite.cpp
@@ -13,7 +13,7 @@ TextSprite::TextSprite(const std::string& text, Object2D* parent, SceneGraph::Dr
     _glyphCache = Manager::instance().get<Text::GlyphCache>("glyph-cache");
     _shader = Manager::instance().get<AbstractShaderProgram, Shaders::Vector2D>("text");
 
-    Rectangle rect;
+    Magnum::Rectangle rect; /* WINAPI I hate you. */
     std::tie(_mesh, rect) = Text::TextRenderer2D::render(*font, *_glyphCache, 8.0f, text, _vertexBuffer, _indexBuffer, Buffer::Usage::StaticDraw);
     translate(Vector2i(rect.size())/-2);
 }
