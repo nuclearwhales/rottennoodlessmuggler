d27b798fbf760f91e6a7ebf9e45e35b46b3c8d74
bageta <owde@seznam.cz>
2013-11-04 21:56:23 +0100

    Added player's avatar in GameScreen

--- src/Player.cpp
+++ src/Player.cpp
@@ -2,7 +2,7 @@
 
 namespace Rotten {
 
-Player::Player(Object2D* parent, SceneGraph::DrawableGroup2D* drawables): Sprite(parent, drawables) {}
+Player::Player(Object2D* parent, SceneGraph::DrawableGroup2D* drawables): Sprite({32,48},"smuggler",parent, drawables) {}
 
 void Player::goLeft(PositionState) {}
 
